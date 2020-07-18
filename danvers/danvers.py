2331db90f4cf3999e0a116d3fc3ce07542e083d7
Justin Joyce <justin.joyce@joocer.com>
2020-07-18 22:16:53 +0100

    checks all previous versions

--- danvers/danvers.py
+++ danvers/danvers.py
@@ -40,7 +40,7 @@ class Danvers:
     def get_data_file(self, dataset, version='latest'):
         dataset_config = self.read_dataset(dataset)
         if version == 'latest':
-            version, version_dict = self._get_latest_version(dataset_config)
+            version = self._get_last_updated_version(dataset_config)
         filename = self._get_filename_from_version(dataset_config, version)
         filepath = os.path.join(self.location, dataset, filename)
         if not os.path.isfile(filepath):
@@ -48,15 +48,29 @@ class Danvers:
         return filepath
 
 
-    def create_dataset(self, dataset, description=''):
+    def create_dataset(self, dataset, description='', max_versions=-1):
+        """
+        Create a new dataset - warns if set already exists.
+
+        Parameters:
+            dataset: name of the dataset
+            description: (optional) a long name for the dataset
+            max_versions: (options) the number of versions to keep (-1 = infinite)
+
+        Returns:
+            Nothing
+        """
         config = { 
             "set": dataset,
             "description": description,
+            "max_versions": max_versions,
             "versions": []
         }
         path = os.path.join(self.location, dataset)
         os.makedirs(path, exist_ok=True)
         filepath = os.path.join(path, 'danvers.json')
+        if os.path.exists(filepath):
+            warnings.warn("Data set (" + dataset + ") already exists.", UserWarning)
         with open(filepath, 'w') as outfile:
             json.dump(config, outfile, ensure_ascii=False, indent=4)
 
@@ -70,14 +84,14 @@ class Danvers:
         """
         config = self.read_dataset(dataset)
         file_hash = self._hash_file(file)
-        try:
-            version, latest_version = self._get_latest_version(config)
-            if file_hash == latest_version['hash']:
-                return version
-            latest_version = version
-        except:
+        #try:
+        version = self._get_matching_version(config, file_hash)
+        if version != 0:
+            return version
+        version = self._get_last_version(config)
+        #except:
             # we're here if there are not existing versions
-            version = 0
+        #    version = 0
 
         extension = os.path.splitext(file)[1]
         filename = file_hash[0:16] + extension
@@ -88,7 +102,8 @@ class Danvers:
         item = {
             "filename": filename,
             "version": version + 1,
-            "timestamp": datetime.datetime.now().isoformat(),
+            "first_added": datetime.datetime.now().isoformat(),
+            "last_added":datetime.datetime.now().isoformat(),
             "hash": file_hash
         }
 
@@ -98,23 +113,43 @@ class Danvers:
         return version + 1
 
 
-    def _get_latest_version(self, config):
-        version = 0
+    def _get_last_updated_version(self, config):
+        EPOCH = datetime.datetime(1970, 1, 1)
+        last_added = EPOCH
         version_dict = {}
+        for item in config['versions']:
+            if datetime.datetime.fromisoformat(item['last_added']) > last_added:
+                last_added = datetime.datetime.fromisoformat(item['last_added'])
+                version_dict = item
+        if version_dict == {}:
+            raise Exception('No versions found')
+        return version_dict['version']
+
+
+    def _get_last_version(self, config):
+        version = 0
         for item in config['versions']:
             if item['version'] > version:
                 version = item['version']
-                version_dict = item
-        if version == 0:
-            raise Exception('No versions found')
-        return version, version_dict
+        return version
+
+
+    def _get_matching_version(self, config, hash):
+        """
+        Searches through previous versions for versions matching hashes.
+        """
+        version = 0
+        for item in config['versions']:
+            if item['hash'] == hash:
+                version = item['version']
+        return version   
 
 
     def _get_filename_from_version(self, config, version):
         for item in config['versions']:
             if item['version'] == version:
                 return item['filename']
-        raise Exception('Version not found')
+        raise 0
 
 
     def _hash_file(self, filename):
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from danvers import Danvers
+import time
 
 # instantiate with the location the data is stored
 vers = Danvers(r'data')
@@ -11,18 +12,22 @@ if not dataset in vers.read_datasets():
 # add the first data file, should return verion 1
 version = vers.create_data_file(dataset, r'test_data\movies_phase_1.csv')
 print(version, vers.get_data_file(dataset))
+time.sleep(1)
 
 # adding the same data file should return version 1 again
 version = vers.create_data_file(dataset, r'test_data\movies_phase_1.csv')
 print(version, vers.get_data_file(dataset))
+time.sleep(1)
 
 # adding a new data file should return version 2
 version = vers.create_data_file(dataset, r'test_data\movies_phase_1+2.csv')
 print(version, vers.get_data_file(dataset))
+time.sleep(1)
 
 # adding another new data file should return version 3
 version = vers.create_data_file(dataset, r'test_data\movies_phase_1+2+3.csv')
 print(version, vers.get_data_file(dataset))
+time.sleep(1)
 
 # getting specific versions, the filenames should match the instances above
 print(1, vers.get_data_file(dataset, 1))
